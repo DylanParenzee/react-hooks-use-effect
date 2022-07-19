b10a5bd861b1d3228185bef5ffc341fefa66d20d
DylanParenzee <dylanparenzee@hotmail.com>
2022-07-19 19:46:21 +1000

    completyetd assignment

--- src/App.js
+++ src/App.js
@@ -1,13 +1,33 @@
 import React, { useState, useEffect } from "react";
 
 function App() {
+  const [count, setCount] = useState(0);
+  const [text, setText] = useState("");
   useEffect(() => {
     console.log("useEffect called");
   });
 
+  const updateCounter = () => {
+    setCount(count + 1);
+  };
+
+  const updateText = (e) => {
+    setText(e.target.value);
+  };
+
   console.log("Component rendering");
 
-  return <button>Click Me</button>;
+  return (
+    <div>
+      <button onClick={updateCounter}>ive been clicked {count} times</button>
+      <input
+        type="text"
+        placeholder="type away..."
+        value={text}
+        onChange={updateText}
+      />
+    </div>
+  );
 }
 
 export default App;
